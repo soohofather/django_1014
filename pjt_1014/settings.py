7650a295001921fbe086704a573fa56df197cfb5
hjkim1350 <hjkim1350@daum.net>
2022-10-14 17:11:35 +0900

    현정-충돌난것 업로드 테스트

--- pjt_1014/settings.py
+++ pjt_1014/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,5 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 # img upload
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = '/media'
+# MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_URL = '/media'

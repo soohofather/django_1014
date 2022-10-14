4459578e61c029146fe6ff95fb34c02312a52b11
soohofather <enthchal@gmail.com>
2022-10-14 12:02:32 +0900

    동현 점심시간 전 커밋

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
 def index(request):
     return render(request, 'accounts/index.html')
+
+def signup(request):
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/signup.html', context=context)
+
+def login(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            if request.GET.get('next'):
+                return redirect(request.GET.get('next'))
+            else:
+                return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/login.html', context)

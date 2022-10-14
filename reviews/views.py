e5ffe0db88bbb6505c9dd2a9d1f3735a0d5b4d0f
hjkim1350 <hjkim1350@daum.net>
2022-10-14 13:09:31 +0900

    reviews 게시판 목록, 글쓰기 구현

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from urllib.request import Request
+from django.shortcuts import render, redirect
+from .models import Review
+from reviews.forms import ReviewForm
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'reviews/index.html')
+    reviews = Review.objects.order_by('-pk')
+
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'reviews/index.html', context)
+
+def create(request):
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect('reviews:index')
+    else:
+        review_form = ReviewForm()
+    
+    context = {
+        'review_form': review_form
+    }
+
+    return render(request, 'reviews/new.html', context)
